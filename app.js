46e88bc00de8b5b6116927e9a8976b0d938cb0b0
Sakon Plangklang <iamsabell@gmail.com>
2025-09-20 22:18:27 +0700

    Using express router and define callback functions for each route

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ import path from 'path';
 import express from 'express';
 
 const app = express();
+const router = express.Router();
 
 app.use(express.json());
+app.use('/api/v1/users', router);
 
 const USERS_FILE = path.join(import.meta.dirname, 'users.json');
 
@@ -26,14 +28,14 @@ const saveUsers = (data) => {
 
 const users = loadUsers();
 
-app.get('/api/v1/users', (req, res) => {
+const getUsers = (req, res) => {
   res.status(200).json({
     status: 'success',
     users,
   });
-});
+};
 
-app.post('/api/v1/users', (req, res) => {
+const postUsers = (req, res) => {
   const newUser = req.body;
   users.push(newUser);
   saveUsers(users);
@@ -42,9 +44,9 @@ app.post('/api/v1/users', (req, res) => {
     message: 'User has been created!',
     user: newUser,
   });
-});
+};
 
-app.patch('/api/v1/users/:id', (req, res) => {
+const updateUsers = (req, res) => {
   const userId = Number(req.params.id);
   const filterUsers = users.filter((u) => u.id !== userId);
   const newUsers = [...filterUsers, req.body];
@@ -54,9 +56,9 @@ app.patch('/api/v1/users/:id', (req, res) => {
     message: 'User has been updated',
     data: req.body,
   });
-});
+};
 
-app.delete('/api/v1/users/:id', (req, res) => {
+const deleteUsers = (req, res) => {
   const userId = parseInt(req.params.id);
   const userToDeletedArr = users.filter((u) => u.id === userId);
   const userToDeleted = userToDeletedArr[0];
@@ -66,6 +68,14 @@ app.delete('/api/v1/users/:id', (req, res) => {
     status: 'success',
     message: `user ${userToDeleted.name} with the ID ${userToDeleted.id} has been deleted!`,
   });
-});
+};
+
+router.route('/').get(getUsers).post(postUsers);
+router.route('/:id').patch(updateUsers).delete(deleteUsers);
+
+// app.get('/api/v1/users', getUsers);
+// app.post('/api/v1/users', postUsers);
+// app.patch('/api/v1/users/:id', updateUsers);
+// app.delete('/api/v1/users/:id', deleteUsers);
 
 export default app;
