68444d1a7f3cd39d27ab0fceb84fcd1286464ff2
Sakon Plangklang <iamsabell@gmail.com>
2025-09-21 19:57:19 +0700

    Create environment variables

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv';
 import express from 'express';
-import router from './routes/usersRoutes';
+import router from './routes/usersRoutes.js';
 
+dotenv.config({ path: './config.env' });
 const app = express();
 
 app.use(express.json());
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import app from './app.js';
 const port = 8000;
 
+console.log(process.env);
+
 app.listen(port, (err) => {
   console.log(`Listening on port ${port}`);
 });
